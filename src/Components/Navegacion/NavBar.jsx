0a7f3323d07285e825bdfc0cd05a7d13c4256640
Marianolavarra21817 <62063011+Marianolavarra21817@users.noreply.github.com>
2022-03-24 20:26:05 -0300

    Add files via upload

--- src/Components/Navegacion/NavBar.jsx
+++ src/Components/Navegacion/NavBar.jsx
@@ -5,13 +5,13 @@ const NavBar = () => {
   <>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
    
-        <img  src="./INNOVAC.jpg" width='50' ></img>
+        <img  src="./INNOVAC.jpg" width='200' ></img>
     
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+    <div class="navbar-nav mx-auto">
       <Link class="nav-link" to ='/'> Inicio </Link>
       <Link class="nav-link" to='/Coach'> Coach</Link>
       <Link class="nav-link" to='/Items' > Items</Link>
